3ea08dac10f60aca8be389420db479fa2f27afc2
jeremyPWE <jpwijanto@gmail.com>
2023-02-08 14:35:59 +0700

    separate data from page

---
+++ data/listData.js
@@ -0,0 +1,108 @@
+const brandingData = {
+  title: "Featured Branding",
+  content: [
+    {
+      imgSrc: "/branding/icon/mr.png",
+      imgTitle: "Morning Rooster",
+      imgDesc:
+        "Morning Rooster is a small roastery, where it intends to expand their businesses by selling a high quality coffee beans and coffee drip product.",
+      link: "/work/morning-rooster",
+    },
+    {
+      imgSrc: "/branding/icon/scru.png",
+      imgTitle: "Scruffy",
+      imgDesc:
+        "Our first exploration with a goal to learn about brand & logo design. Scruffy is a brand that provide sustainable pet care product for your fur buddy.",
+      link: "/work/scruffy",
+    },
+  ],
+};
+
+const websiteData = {
+  title: "Featured Websites",
+  content: [
+    {
+      imgSrc: "/website/icon/gk.png",
+      imgTitle: "GALERI KERAMIK",
+      imgDesc:
+        "Bringing together Galeri Keramik different products category, sevices, contact into an unified website experience.",
+      link: "/work/galeri-keramik",
+    },
+    {
+      imgSrc: "/website/icon/cm.png",
+      imgTitle: "COMPOSURE MAGAZINE",
+      imgDesc:
+        "We did a website redesign for Composure Magazine, an online magazine based in USA. Composure Magazine is a publication that celebrates current fashion and beauty trends, while showcasing the style and lives of celebrities.",
+      link: "/work/composure-magazine",
+    },
+    {
+      imgSrc: "/website/icon/fc.png",
+      imgTitle: "FOCUS CULT",
+      imgDesc:
+        "Focus cult is an initiative answer to lessen the impact of smarphone addiction by making its user close down the phone and record their choosen activity. (UI/UX Research)",
+      link: "/work/focus-cult",
+    },
+    {
+      imgSrc: "/website/icon/al.png",
+      imgTitle: "AVATERRA LIVING",
+      imgDesc:
+        "We teamed up with the people from Avaterra Living to design and create their first ever website. Avaterra living is a company with a mission and vision to promote the best handicrafts from local Indonesian artisans.",
+      link: "/work/avaterra-living",
+    },
+    {
+      imgSrc: "/website/icon/gate.png",
+      imgTitle: "GATE KEEPING",
+      imgDesc:
+        "This section is our take on how art gallery should adapt during pandemic, and that adaptation is to utilize online platform to make sure that people could visit or see the art on their offline gallery. ",
+      link: "/work/gate-keeping",
+    },
+    {
+      imgSrc: "/website/icon/betu.png",
+      imgTitle: "BETUNICA",
+      imgDesc:
+        "Creating fictional website for a luxury dinner type restaurant. We design everything from start to finish.(Logo, typography, color template, web design)",
+      link: "/work/betunica",
+    },
+  ],
+};
+
+const otherData = {
+  content: [
+    {
+      imgSrc: "/website/icon/celco.png",
+      imgTitle: "CELCO",
+      imgDesc:
+        "Celco Indonesia is a leading brand that processes the best quality of coconut products in the form of charcoal briquettes in Indonesia.",
+      link: "/work/celco",
+    },
+    {
+      imgSrc: "/website/icon/a-plus.png",
+      imgTitle: "A-PLUS",
+      imgDesc:
+        "In this challege we tried to create a stand alone landing page in our own creative way.",
+      link: "/work/a-plus",
+    },
+    {
+      imgSrc: "/website/icon/reck.png",
+      imgTitle: "RECK-ON",
+      imgDesc: "This is our take on digital travel agency landing page.",
+      link: "/work/reck-on",
+    },
+    {
+      imgSrc: "/website/icon/sl.png",
+      imgTitle: "SURAJA LAW",
+      imgDesc:
+        "SURAJA Law Offices is a Jakarta based law firm offering a full range of Indonesian legal services.",
+      link: "/work/suraja-law",
+    },
+    {
+      imgSrc: "/website/icon/travellux.png",
+      imgTitle: "TRAVELLUX",
+      imgDesc:
+        "Travellux is our take on a digital travel journals where travellers could share their personal travel experiences.",
+      link: "/work/travellux",
+    },
+  ],
+};
+
+export { brandingData, websiteData, otherData };
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,6 +1,8 @@
 import Header from "../../components/Header";
 import Layout from "../../components/Layout";
 import Section from "../../components/Section";
+import { brandingData, websiteData, otherData } from "../../data/listData";
+
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Image from "next/image";
@@ -74,7 +76,9 @@ const services = () => {
 };
 
 const allProject = () => {
-  const all = brandingData.content.concat(websiteData.content);
+  const all = brandingData.content
+    .concat(websiteData.content)
+    .concat(otherData.content);
   return (
     <div className="flex flex-col w-full">
       <h1>All Project</h1>
@@ -87,7 +91,7 @@ const allProject = () => {
                 <Image
                   src={item.imgSrc}
                   fill
-                  className="object-contain"
+                  className="object-cover"
                   alt={`${item.imgTitle} image`}
                 />
               </div>
@@ -101,74 +105,6 @@ const allProject = () => {
       </div>
     </div>
   );
-};
-
-const brandingData = {
-  title: "Featured Branding",
-  content: [
-    {
-      imgSrc: "/branding/icon/mr.png",
-      imgTitle: "Morning Rooster",
-      imgDesc:
-        "Morning Rooster is a small roastery, where it intends to expand their businesses by selling a high quality coffee beans and coffee drip product.",
-      link: "/work/morning-rooster",
-    },
-    {
-      imgSrc: "/branding/icon/scru.png",
-      imgTitle: "Scruffy",
-      imgDesc:
-        "Our first exploration with a goal to learn about brand & logo design. Scruffy is a brand that provide sustainable pet care product for your fur buddy.",
-      link: "/work/scruffy",
-    },
-  ],
-};
-
-const websiteData = {
-  title: "Featured Websites",
-  content: [
-    {
-      imgSrc: "/website/icon/gk.png",
-      imgTitle: "GALERI KERAMIK",
-      imgDesc:
-        "Bringing together Galeri Keramik different products category, sevices, contact into an unified website experience.",
-      link: "/work/galeri-keramik",
-    },
-    {
-      imgSrc: "/website/icon/cm.png",
-      imgTitle: "COMPOSURE MAGAZINE",
-      imgDesc:
-        "We did a website redesign for Composure Magazine, an online magazine based in USA. Composure Magazine is a publication that celebrates current fashion and beauty trends, while showcasing the style and lives of celebrities.",
-      link: "/work/composure-magazine",
-    },
-    {
-      imgSrc: "/website/icon/fc.png",
-      imgTitle: "FOCUS CULT",
-      imgDesc:
-        "Focus cult is an initiative answer to lessen the impact of smarphone addiction by making its user close down the phone and record their choosen activity. (UI/UX Research)",
-      link: "/work/focus-cult",
-    },
-    {
-      imgSrc: "/website/icon/al.png",
-      imgTitle: "AVATERRA LIVING",
-      imgDesc:
-        "We teamed up with the people from Avaterra Living to design and create their first ever website. Avaterra living is a company with a mission and vision to promote the best handicrafts from local Indonesian artisans.",
-      link: "/work/avaterra-living",
-    },
-    {
-      imgSrc: "/website/icon/gate.png",
-      imgTitle: "GATE KEEPING",
-      imgDesc:
-        "This section is our take on how art gallery should adapt during pandemic, and that adaptation is to utilize online platform to make sure that people could visit or see the art on their offline gallery. ",
-      link: "/work/gate-keeping",
-    },
-    {
-      imgSrc: "/website/icon/betu.png",
-      imgTitle: "BETUNICA",
-      imgDesc:
-        "Creating fictional website for a luxury dinner type restaurant. We design everything from start to finish.(Logo, typography, color template, web design)",
-      link: "/work/betunica",
-    },
-  ],
 };
 
 const featuredWork = (data) => {
